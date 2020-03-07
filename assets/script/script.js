27fb442877e9a000680ece1a38de844cb5a0f1b9
Sue Rocha <swissbusybee@gmail.com>
2020-03-07 22:22:03 +0100

    JS Event Listeners Added

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,17 +1,83 @@
 window.onload = function () {
 
-    const displayButtonCalculate = document.querySelectorAll(".btn btn-default");
+    // ADDING TO TABLE FROM INPUT
+
+    function formTable(selector) {
+        var grid = document.querySelector(selector);
+        var form = grid.getElementsByTagName("form")[0];
+        var table = grid.getElementsByTagName("table")[0];
+
+        form.onsubmit = function (ev) {
+            ev = ev || event;
+            ev.preventDefault();
+
+            var tr = document.createElement("tr"), td;
+
+            for (let i = 0; i < 3; i++) {
+                td = document.createElement("td");
+                td.innerHTML = this.elements[i].value;
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
+        }
+    }
+
+    formTable(".grid");
+
+    // TABLE SORT ON BUTTON CLICK 
+    //https://www.geeksforgeeks.org/how-to-sort-rows-in-a-table-using-javascript/
+
+    let clickedButton = document.getElementById("btnclick");
+
+    clickedButton.addEventListener("click", sortTable);
+
+    function sortTable() {
+        let table, i, x, y;
+        table = document.getElementById("maintable");
+        let switching = true;
+
+        // Run loop until no switching is needed
+        while (switching) {
+            switching = false;
+            let rows = table.rows;
+
+            // Loop to go through all rows
+            for (i = 1; i < (rows.length - 1); i++) {
+                var Switch = false;
+
+                // Fetch 2 elements that need to be compared
+                x = rows[i].getElementsByTagName("TD")[0];
+                y = rows[i + 1].getElementsByTagName("TD")[0];
+
+                // Check if 2 rows need to be switched
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+
+                    // If yes, mark Switch as needed and break loop
+                    Switch = true;
+                    break;
+                }
+            }
+            if (Switch) {
+                // Function to switch rows and mark switch as completed
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+            }
+        }
+    }
+
+
+    /*const displayButtonCalculate = document.querySelectorAll(".btn btn-default");
     const selectGrade = document.querySelector('#select-grade');
     const selectYear = document.querySelector('#select-year');
     const selectGradeOveride = document.querySelector('#select-grade-overide');
     const yearTableHeader = document.querySelector('#school-year-table-header');
     const gradeTableHeader = document.querySelector('#grade-table-header');
     const lepTableHeader = document.querySelector('#lep-indicator-code-table-header');
-
+    
     //CALCULATE FUNCTIONS
-
-    const calculate = (grade, year) => {
-        switch (grade) {
+    
+    const calculate = (grade) => {
+        switch (calculate) {
             case 'K': return '1 = EL (BIL/ESL/PD)';
             case 1: return '1 = EL (BIL/ESL/PD)';
             case 2: return '1 = EL (BIL/ESL/PD)';
@@ -28,13 +94,13 @@ window.onload = function () {
             default: return 0;
         }
     };
-
+    
     //DISPLAY & EVENT LISTENER FUNCTIONS
-
+    
     displayButtonCalculate.forEach((button) => {
         button.addEventListener('click', (e) => {
             const val = e.target.value;
         })
-    });
+    });*/
 
 }
